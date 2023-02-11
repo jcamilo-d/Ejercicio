3e86768d4efb547ac981970008c4c5a7bc8047fc
zip-b205-e-002 <jhoan.diaz@uniminuto.edu.co>
2023-02-10 20:05:27 -0500

    Se agregaron variables

--- src/clases/principal.java
+++ src/clases/principal.java
@@ -3,5 +3,10 @@ package clases;
 public class principal {
     public static void main(String[] args) {
         System.out.println("HOLA");
+
+        int a=5, b=6;
+
+        System.out.println("a="+a);
+        System.out.println("b="+b);
     }
 }
