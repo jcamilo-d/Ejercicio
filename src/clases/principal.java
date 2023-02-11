e118b2a8d6244c4c45ae74cc5cd3ca4281f18050
zip-b205-e-002 <jhoan.diaz@uniminuto.edu.co>
2023-02-10 20:21:33 -0500

    Se agregaron variables dobles

--- src/clases/principal.java
+++ src/clases/principal.java
@@ -11,5 +11,7 @@ public class principal {
         System.out.println("b="+b);
         System.out.println("c="+c);
         System.out.println("d="+d);
+
+        System.out.println("LA suma de " +a +" mas " +b +" es: "+(a+b));
     }
 }
